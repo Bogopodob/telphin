6e2d392b4aff1f6b420a659213c9ab708feaefad
Bogopodob <45335821+Bogopodob@users.noreply.github.com>
2022-10-11 17:09:13 +0700

    Update Client.php

--- lib/Methods/Client/Client.php
+++ lib/Methods/Client/Client.php
@@ -1,96 +1,203 @@
 <?php
-namespace Telphin;
-use Telphin\Config;
-use Telphin\Methods;
+namespace Telphin\Methods\Client;
 /**
- * Connection with api telphin
- * Methods - traits
+ * Трейт для пользователя типа клиент, методы не связаны с Агентами
  */
-class Client
+trait Client
 {
-  const METHOD_GET    = 'GET';
-  const METHOD_POST   = 'POST';
-  const METHOD_DELETE = 'DELETE';
-  const METHOD_PUT    = 'PUT';
-  const GRANT_TYPE  = 'client_credentials';
-  public $type = 'client';
-  private $client_id;
-  private $client_secret;
-  private $conf;
-  private $url;
-  private static $version;
-
-  function __construct(string $client_id, string $client_secret)
+  function getClSoundDownload($client_id = "@me", $sound_id)
   {
-    $this->client_id = $client_id;
-    $this->client_secret = $client_secret;
-    $this->conf = (new Config);
-    $this->url = $this->conf->conf("url");
-    self::$version = $this->conf->conf("versionPrefix");
-  }
-
-  private function refreshToken()
-  {
-    $path   = "/oauth/token";
-    $method = "POST";
-    $data   = [
-      "grant_type"    => self::GRANT_TYPE,
-      "client_secret" => $this->client_secret,
-      "client_id"     => $this->client_id
-    ];
-    $result = $this->makeRequest($path, $method, $data);
-    return $result->access_token;
-  }
-
-  private function makeRequest(string $path, string $method, array $data = [], $encode = false, bool $isFile = false)
-  {
-    $curlHandler = curl_init();
-      if (self::METHOD_GET == $method && count($data)) {
-         $path .= "?".http_build_query($data, '', '&');
-      }
-      if ($encode) $data = json_encode($data);
-      curl_setopt($curlHandler, CURLOPT_URL, $this->url.$path);
-      curl_setopt($curlHandler, CURLOPT_RETURNTRANSFER, 1);
-      curl_setopt($curlHandler, CURLOPT_FOLLOWLOCATION, 1);
-      curl_setopt($curlHandler, CURLOPT_FAILONERROR, false);
-      curl_setopt($curlHandler, CURLOPT_SSL_VERIFYPEER, false);
-      curl_setopt($curlHandler, CURLOPT_SSL_VERIFYHOST, false);
-      curl_setopt($curlHandler, CURLOPT_TIMEOUT, $this->conf->conf("timeout"));
-      curl_setopt($curlHandler, CURLOPT_CONNECTTIMEOUT, $this->conf->conf("timeout"));
-      switch ($method) {
-        case self::METHOD_POST:
-          curl_setopt($curlHandler, CURLOPT_POST, 1);
-          curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $data);
-          break;
-        case self::METHOD_DELETE:
-          curl_setopt($curlHandler, CURLOPT_CUSTOMREQUEST, "DELETE");
-          curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $data);
-          break;
-        case self::METHOD_PUT:
-          curl_setopt($curlHandler, CURLOPT_CUSTOMREQUEST, "PUT");
-          curl_setopt($curlHandler, CURLOPT_POSTFIELDS, $data);
-          break;
-      }
-    if ($path != "/oauth/token") {
-      curl_setopt($curlHandler, CURLOPT_HEADER, 0);
-      curl_setopt($curlHandler, CURLOPT_HTTPHEADER, [
-        "Content-Type: application/json",
-        "authorization: Bearer {$this->refreshToken()}"
-      ]);
-    }
-    $result = curl_exec($curlHandler);
-    $codeResponse = curl_getinfo($curlHandler, CURLINFO_RESPONSE_CODE);
-    curl_close($curlHandler);
-    if ($codeResponse == "404") return (object)$this->conf->conf("errors.response404");
-    if (!$isFile)
-      $result = json_decode($result);
-
+    $path = self::$version . "/client/{$client_id}/sound/{$sound_id}/download/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, [], false, true);
     return $result;
   }
 
-  use Methods\Client\Agent;
-  use Methods\Client\Client;
-  use Methods\Extension\Extension;
-  use Methods\Statistics\StatisticsClient;
+  function getClSounds($client_id = "@me", $data = [])
+  {
+    $path = self::$version."/client/{$client_id}/sound/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function getClLimit($client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/limit/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function getClInfo($client_id = "@me")
+  {
+    $path = self::$version."/client/client_info/{$client_id}/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function getClRegStatusLog($data, $order = "desc", $client_id = "@me")
+  {
+    //if(!@$data['start_datetime'] || !@$data['end_datetime']) return $this->conf->conf("errors.paramsRequire");
+    $path = self::$version."/client/{$client_id}/reg_status_log/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function getClAlias($data = [], $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/alias/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function getClDID($data = [], $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/did/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function getClCallHistory($data = [], $call_uuid = "", $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/call_history/{$call_uuid}";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function getClExtension($data = [], int $extension_id = 0, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension/";
+    if(!empty($extension_id)) $path .= "{$extension_id}";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function addClExtension(array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension/";
+    $method = "POST";
+    $result = $this->makeRequest($path, $method, $data, true);
+    return $result;
+  }
+
+  function updClExtension($extension_id, array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension/{$extension_id}";
+    $method = "PUT";
+    $result = $this->makeRequest($path, $method, $data, true);
+    return $result;
+  }
+
+  function delClExtension($extension_id, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension/{$extension_id}";
+    $method = "DELETE";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function getClExtensionGroup($data = [], $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension_group/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function addClExtensionGroup(array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension_group/";
+    $method = "POST";
+    $result = $this->makeRequest($path, $method, $data, true);
+    return $result;
+  }
+
+  function updClExtensionGroup(int $ext_group_id, array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension_group/{$ext_group_id}";
+    $method = "PUT";
+    $result = $this->makeRequest($path, $method, $data, true);
+    return $result;
+  }
+
+  function delClExtensionGroup(int $ext_group_id, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/extension_group/{$ext_group_id}";
+    $method = "DELETE";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+  /* Records block  */
+  function getClRecordList(array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/record/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function getClRecordArchive(array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/record/archive/";
+    $method = "POST";
+    $result = $this->makeRequest($path, $method, $data, true);
+    return $result;
+  }
+
+  function getClRecordStats(array $data, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/record/stats/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method, $data);
+    return $result;
+  }
+
+  function delClRecordByID($record_uuid, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/record/{$record_uuid}/";
+    $method = "DELETE";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function getClRecordByID($record_uuid, $client_id = "@me") // Need tests
+  {
+    $path = self::$version."/client/{$client_id}/record/{$record_uuid}/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function downloadClRecord($record_uuid, $client_id = "@me") // Need tests
+  {
+    $path = self::$version."/client/{$client_id}/record/{$record_uuid}/download/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function getClRecordStorage($record_uuid, $client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/record/{$record_uuid}/storage_url/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
+
+  function getClCurrentCalls($client_id = "@me")
+  {
+    $path = self::$version."/client/{$client_id}/current_calls/";
+    $method = "GET";
+    $result = $this->makeRequest($path, $method);
+    return $result;
+  }
 }
+
 ?>
